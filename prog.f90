efa34df1bda422182b3831c736bbfc014d37604b
momolemomo <bebmomo16@gmail.com>
2026-03-26 22:07:21 +0100

    C'est le prog complet

--- prog.f90
+++ prog.f90
@@ -6,22 +6,25 @@ program main
     type(phys) :: p
     type(num) :: n
     type(grid) :: g
-    real :: d_t
+    real, dimension(:,:), allocatable :: C_futur
+    real :: d_t, Time, Vol, Tf
 
-    integer :: nb_ite, i, j
+    integer :: nb_ite, i, j, Step
 
 
 
     call reader('donnees.dat', p, n)
 
     allocate(g%c(n%nx, n%ny))
+    allocate(C_futur(n%nx, n%ny))
+
     allocate(g%u(n%nx +1 , n%ny))
     allocate(g%v(n%nx, n%ny +1))
 
     allocate(noeud%x(n%nx +1, n%ny +1))
     allocate(noeud%y(n%nx +1, n%ny +1))
 
-
+    Vol = 2* p%l/n%nx * p%l / n%ny
     call init_c(g%c, p, n)
     call init_v(g%u, g%v, p, n)
     call init_noeud(g, p, n, noeud)
@@ -35,14 +38,22 @@ program main
 !    print*, g%v
 !    print*, "on print les coordonnées des noeuds"
 
-    do i = 1, n%nx +1
-        do j = 1, n%ny +1
-!            print*, "pour x", noeud%x(i,j)
-!            print*, "pour y", noeud%y(i,j) 
-        end do
+
+    Time = 0.
+    nb_ite = 1000
+    Tf = (nb_ite-1) * n%dt
+    Step = 1
+
+
+    do i = 1, nb_ite
+        call writer(n, p, g, noeud, Time, Tf, Step)
+        call calc_c_t_dt(C_futur, g, d_t, Vol, p, n) 
+        g%c = C_futur
+        Time = Time + n%dt
+        Step = Step + 1
+
     end do
 
-    call writer(n, p, g, noeud, 1)
 
     
 
